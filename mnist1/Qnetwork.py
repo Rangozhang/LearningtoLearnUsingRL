a1e238b48d8403ece2e3a803c9d9f72f8b41e4ed
yu01.zhang <yu01.zhang@yz-gpu029.hogpu.cc>
2017-02-16 14:10:46 +0800

    softmaxRegression+RL

--- mnist1/Qnetwork.py
+++ mnist1/Qnetwork.py
@@ -9,7 +9,7 @@ tf.app.flags.DEFINE_integer('num_actions', 2, 'number of actions')
 tf.app.flags.DEFINE_integer('num_episodes', 100, 'number of episodes')
 tf.app.flags.DEFINE_integer('len_episodes', 550, 'length of an episode')
 tf.app.flags.DEFINE_integer('batch_size', 32, '')
-tf.app.flags.DEFINE_integer('observ_dim', 200, 'dimension of state')
+tf.app.flags.DEFINE_integer('observ_dim', 35, 'dimension of state')
 tf.app.flags.DEFINE_integer('state_num', 4, 'number of observ are stacked in a state')
 tf.app.flags.DEFINE_float('gamma', 0.99, 'discount factor on past Q values')
 tf.app.flags.DEFINE_float('start_e', 1.0, 'chance of random action at the beginning')
@@ -18,10 +18,10 @@ tf.app.flags.DEFINE_float('lr', 1e-3, 'learning rate')
 tf.app.flags.DEFINE_boolean('isTraining', True, 'is training?')
 
 # uncertain
-tf.app.flags.DEFINE_integer('explore', 10000, 'observe before training')
-tf.app.flags.DEFINE_integer('observe', 64, 'observe before training') # as long as > batch_size
+tf.app.flags.DEFINE_integer('explore', 60000, 'observe before training')
+tf.app.flags.DEFINE_integer('observe', 30000, 'observe before training') # as long as > batch_size
 tf.app.flags.DEFINE_float('tau', 1e-3, 'rate to update target network towards primary network')
-tf.app.flags.DEFINE_integer('memory_size', 2000, 'replay memory size')
+tf.app.flags.DEFINE_integer('memory_size', 30000, 'replay memory size')
 tf.app.flags.DEFINE_float('training_freq', 2, 'How ofen to perform a trianing step')
 
 def linear(input_, output_size, stddev=0.02, bias_start=0.0, activation_fn=None, name='linear'):
@@ -55,7 +55,6 @@ class Qnetwork(object):
     self.step = 0
     self.batch_size = FLAGS.batch_size
     self.epsilon = FLAGS.start_e
-    self.sess = tf.Session()
     with tf.variable_scope('primary'):
       self.state_input, self.Q, self.w = self.create_model()
     with tf.variable_scope('target'):
@@ -64,17 +63,24 @@ class Qnetwork(object):
       self.w_t_input = {}
       self.w_t_update_op = {}
       for name in self.w_t.keys():
-        self.w_t_update_op[name] = self.w_t[name].assign(tau*self.w[name], (1-tau)*self.w_t[name])
-        #self.w_t_update_op[name] = self.w_t[name].assign(tau*self.w[name].value(), (1-tau)*self.w_t[name].value())
+        self.w_t_update_op[name] = self.w_t[name].assign(FLAGS.tau*self.w[name]+(1-FLAGS.tau)*self.w_t[name])
+        #self.w_t_update_op[name] = self.w_t[name].assign(FLAGS.tau*self.w[name].value(), (1-FLAGS.tau)*self.w_t[name].value())
         #self.w_t_input[name] = tf.placeholder('float32', self.w_t[name].get_shape().as_list(), name=name)
         #self.w_t_update_op[name] = self.w_t[name].assign(self.w_t_input[name])
-    self.action_pred = argmax(self.Q, 1)
+    self.action_pred = tf.argmax(self.Q, 1)
     self.create_train_op()
+
+    config_proto = tf.ConfigProto(log_device_placement=False)
+    # config_proto.gpu_options.per_process_gpu_memory_fraction = 0.4
+    config_proto.gpu_options.allow_growth = True
+    # config_proto.gpu_options.visible_device_list = '1'
+    self.session = tf.Session(config=config_proto)
+    self.session.run(tf.global_variables_initializer())
 
   def create_model(self):
     W = {}
     state_input = tf.placeholder('float32', [None, FLAGS.observ_dim*FLAGS.state_num])
-    l1, W['l1_w'], W['l1_b'] = linear(state_input, 512, \
+    l1, W['l1_w'], W['l1_b'] = linear(state_input, 1024, \
             activation_fn=tf.nn.relu, name='l1')
     l2, W['l2_w'], W['l2_b'] = linear(l1, 512, \
             activation_fn=tf.nn.relu, name='l2')
@@ -83,7 +89,7 @@ class Qnetwork(object):
     adv_hid, W['adv_hid_w'], W['adv_hid_b'] = linear(l2, 256, \
             activation_fn=tf.nn.relu, name='adv_hid')
     v, W['v_w'], W['v_b'] = linear(v_hid, 1, name='v')
-    adv, W['adv_w'], w['adv_b'] = linear(adv_hid, FLAGS.num_actions, name='adv')
+    adv, W['adv_w'], W['adv_b'] = linear(adv_hid, FLAGS.num_actions, name='adv')
     Q = v + (adv - tf.reduce_mean(adv, reduction_indices=1, keep_dims=True))
     return state_input, Q, W
 
@@ -103,14 +109,14 @@ class Qnetwork(object):
     self.trainer = tf.train.AdamOptimizer(learning_rate=FLAGS.lr).minimize(self.cost)
 
   def init_state(self, observation):
-    assert (observation.size == 2), "Invalid observation!"
     obvs = []
     for i in xrange(FLAGS.state_num):
       obvs.append(observation)
-    self.cur_state = np.concatenate(obvs, axis=1)
+    self.cur_state = np.hstack(obvs)
+    assert (self.cur_state.ndim == 2), "Invalid initial state"
 
   def set_perception(self, action, reward, next_observ, terminal):
-    assert (next_observ.size == 2), "Invalid next observation"
+    assert (next_observ.ndim == 2), "Invalid next observation 2 vs. {}".format(next_observ.ndim)
 
     ### 1. update memory
     next_state = np.append(self.cur_state[:, FLAGS.observ_dim:], next_observ, axis=1)
@@ -122,49 +128,61 @@ class Qnetwork(object):
 
     ### 3. print info.
     state = ""
-    if self.timeStep <= OBSERVE:
+    if self.step <= FLAGS.observe:
         state = "observe"
     elif self.step > FLAGS.observe and self.step <= FLAGS.observe + FLAGS.explore:
         state = "explore"
     else:
         state = "train"
 
-    print "TIMESTEP", self.step, "/ STATE", state, \
-          "/ EPSILON", self.epsilon
+    state_str = 'state=' + state + ' e={:.5f}'.format(self.epsilon)
 
     ### 4. update training states
     self.cur_state = next_state
     self.step += 1
+    return state_str
 
   def train(self):
     ### 1. obtain training batch from memory
     minibatch = self.memory.sample(self.batch_size)
-    state_batch = np.stack([data[0] for data in minibatch])
+    state_batch = np.vstack([data[0] for data in minibatch])
     action_batch = np.stack([data[1] for data in minibatch])
     reward_batch = np.stack([data[2] for data in minibatch])
-    nx_state_batch = np.stack([data[3] for data in minibatch])
+    nx_state_batch = np.vstack([data[3] for data in minibatch])
     terminal = np.stack([data[4] for data in minibatch])
-    assert (state_batch.size == 2 and action_batch.size == 2 and \
-            reward_batch.size == 2 and nx_state_batch.size == 2 and \
-            terminal.size == 1), "Invalid training input"
+    # print state_batch.shape
+    # print action_batch.shape
+    # print reward_batch.shape
+    # print nx_state_batch.shape
+    # print terminal.shape
+    assert (state_batch.ndim == 2 and action_batch.ndim == 2 and \
+            reward_batch.ndim == 1 and nx_state_batch.ndim == 2 and \
+            terminal.ndim == 1), "Invalid training input"
 
     ### 2. calculate Q_gt
     action_pred = self.session.run([self.action_pred], \
-            feed_dict={self.state_input: nx_state_batch})
+            feed_dict={self.state_input: nx_state_batch})[0]
     q_pred_t = self.session.run([self.Q_t], \
-            feed_dict={self.state_input_t: nx_state_batch})
+            feed_dict={self.state_input_t: nx_state_batch})[0]
     terminal_multiplier = -(terminal-1)
     double_q = q_pred_t[range(FLAGS.batch_size), action_pred]
     Q_gt = reward_batch + FLAGS.gamma * double_q * terminal_multiplier
     self.session.run([self.trainer], feed_dict={
-                        self.state_input: state_bach,
+                        self.state_input: state_batch,
                         self.Q_gt: Q_gt,
                         self.action: action_batch})
     self.update_target_network()
 
-  def proc_state(self, state):
+  def get_action(self):
+    action_pred = self.session.run([self.action_pred], \
+            feed_dict={self.state_input: self.cur_state})[0]
+    action = np.zeros(FLAGS.num_actions)
+    action_ind = action_pred if random.random() > self.epsilon \
+                             else random.randrange(FLAGS.num_actions)
+    action[action_ind] = 1
 
+    if self.epsilon > FLAGS.end_e and self.step > FLAGS.observe:
+        self.epsilon -= (FLAGS.start_e - FLAGS.end_e)/FLAGS.explore
+    return action
 
-
-if __name__ == "__main__":
-  raw_input()
+  #def proc_state(self, state):
